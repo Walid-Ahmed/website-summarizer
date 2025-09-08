2b6dd3f3719b2be9bc0f6e7c7536e8b48eb291a2
Walid Ahmed <walmaly@gmail.com>
2025-09-08 06:38:11 -0400

    Add files via upload

---
+++ summarizer_with_ollama.py
@@ -0,0 +1,51 @@
+
+from IPython.display import Markdown, display
+import ollama
+from website import  Website
+
+
+# Constants
+
+MODEL = "llama3.2"
+
+
+
+
+
+# Define our system prompt - you can experiment with this later, changing the last sentence to 'Respond in markdown in Spanish."
+
+system_prompt = "You are an assistant that analyzes the contents of a website \
+and provides a short summary, ignoring text that might be navigation related. \
+Respond in markdown."
+
+# A function that writes a User Prompt that asks for summaries of websites:
+
+def user_prompt_for(website):
+    user_prompt = f"You are looking at a website titled {website.title}"
+    user_prompt += "The contents of this website is as follows; \
+please provide a short summary of this website in markdown. \
+If it includes news or announcements, then summarize these too.\n\n"
+    user_prompt += website.text
+    return user_prompt
+
+# See how this function creates exactly the format above
+
+def messages_for(website):
+    return [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": user_prompt_for(website)}
+    ]
+
+
+def summarize(url):
+    website = Website(url)
+    messages = messages_for(website)
+    response = ollama.chat(model=MODEL, messages=messages)
+    return response['message']['content']
+
+
+website="https://cnn.com"
+response=summarize(website)
+print(response)
+
+
---
+++ summarizer_with_openai.py
@@ -0,0 +1,47 @@
+
+from dotenv import load_dotenv
+import os
+from website import  Website
+from openai import OpenAI
+
+load_dotenv(override=True)
+api_key = os.getenv('OPENAI_API_KEY')
+openai = OpenAI()
+
+# Define our system prompt - you can experiment with this later, changing the last sentence to 'Respond in markdown in Spanish."
+
+system_prompt = "You are an assistant that analyzes the contents of a website \
+and provides a short summary, ignoring text that might be navigation related. \
+Respond in markdown."
+
+# A function that writes a User Prompt that asks for summaries of websites:
+
+def user_prompt_for(website):
+    user_prompt = f"You are looking at a website titled {website.title}"
+    user_prompt += "The contents of this website is as follows; \
+please provide a short summary of this website in markdown. \
+If it includes news or announcements, then summarize these too.\n\n"
+    user_prompt += website.text
+    return user_prompt
+
+# See how this function creates exactly the format above
+
+def messages_for(website):
+    return [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": user_prompt_for(website)}
+    ]
+
+
+
+def summarize(url):
+    website = Website(url)
+    response = openai.chat.completions.create(
+        model = "gpt-4o-mini",
+        messages = messages_for(website)
+    )
+    return response.choices[0].message.content
+
+website="https://cnn.com"
+response=summarize(website)
+print(response)
